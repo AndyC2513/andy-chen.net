21f351425fe1b61c7af5ce4929c12ab71c3478b5
AndyC <ychen276@students.cs.ubc.ca>
2024-07-26 15:41:48 -0700

    finished 3d models, starting to refactor

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,6 +8,7 @@ import Plane from "../models/Plane";
 
 const Home = () => {
   const [isRotating, setIsRotating] = useState(false);
+  const [currentStage, setCurrentStage] = useState(1);
 
   const adjustIslandForScreenSize = () => {
     let screenScale;
@@ -58,7 +59,9 @@ const Home = () => {
               intensity={1}
             />
 
-            <Sky />
+            <Sky 
+              isRotating={isRotating}  
+            />
             <Bird />
             <Island
               position={islandPosition}
@@ -66,6 +69,7 @@ const Home = () => {
               rotation={islandRotation}
               isRotating={isRotating}
               setIsRotating={setIsRotating}
+              setCurrentStage={setCurrentStage}
             />
             <Plane
               position={planePosition}
